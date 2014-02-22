1ac836301478c81c2dbc411896e3029ee09bd71f
Adam Comley <adam@novators.net>
2014-02-22 10:26:15 -0600

    Support for Windows (win32) platform

--- doc/html/search/functions_65.js
+++ doc/html/search/functions_65.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['enscrypt',['enscrypt',['../enscrypt_8h.html#a26eac647cd1afb26a47e2e57c2b374e3',1,'enscrypt.h']]],
-  ['enscrypt_5fget_5freal_5ftime',['enscrypt_get_real_time',['../enscrypt_8h.html#a996d6e7d162d0b1b6e505f617c4734b1',1,'enscrypt.h']]],
-  ['enscrypt_5fms',['enscrypt_ms',['../enscrypt_8h.html#a636d6c629744e1573a00258ffe913cab',1,'enscrypt.h']]],
-  ['enscrypt_5fset_5ffatal_5ferror',['enscrypt_set_fatal_error',['../enscrypt_8h.html#a759cb634b742b2f4c2c5da7c01342e95',1,'enscrypt.h']]]
+  ['enscrypt',['enscrypt',['../enscrypt_8h.html#ae7753c67103557eed29024f61ae30935',1,'enscrypt.h']]],
+  ['enscrypt_5fget_5freal_5ftime',['enscrypt_get_real_time',['../enscrypt_8h.html#ad45d27f5446bc67a038e6a4394562641',1,'enscrypt.h']]],
+  ['enscrypt_5fms',['enscrypt_ms',['../enscrypt_8h.html#a6d2b1424b924ace8c56b8e0dae1a9d6c',1,'enscrypt.h']]],
+  ['enscrypt_5fset_5ffatal_5ferror',['enscrypt_set_fatal_error',['../enscrypt_8h.html#af1097544078b7561dcf00d975b0ad7cf',1,'enscrypt.h']]]
 ];
